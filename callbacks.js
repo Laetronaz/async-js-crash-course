c8f2509135ec2ac56e7bf72cedfa7963581e5c40
Michel Pelland <michel.pelland12@gmail.com>
2019-04-07 14:15:33 -0400

    Finished Tutorial

---
+++ callbacks.js
@@ -0,0 +1,23 @@
+const posts = [
+  { title: "Post One", Body: "This is post one." },
+  { title: "Post Two", Body: "This is post rwo." }
+];
+
+function getPosts() {
+  setTimeout(() => {
+    let output = "";
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPost(post, callback) {
+  setTimeout(() => {
+    posts.push(post);
+    callback();
+  }, 2000);
+}
+
+createPost({ title: "Post Three", body: "this is post three" }, getPosts);
